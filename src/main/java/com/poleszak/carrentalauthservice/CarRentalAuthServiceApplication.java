0975452f90972f72e719484657a579d3ff5c972e
piotrpoleszak <poleszak@protonmail.com>
2023-03-24 15:49:57 +0100

    Initial commit

---
+++ src/main/java/com/poleszak/carrentalauthservice/CarRentalAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.poleszak.carrentalauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poleszak/carrentalauthservice/CarRentalAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poleszak.carrentalauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
