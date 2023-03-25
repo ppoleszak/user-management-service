2d1b564fd9fa31bdee3c2c575691c2cc03039ca6
null <91702209+piotrpoleszak@users.noreply.github.com>
2023-03-25 03:00:17 +0100

    001-prepare_packages_structure (#1)
    * Prepare config files and classes in project
    
    * Create configuration for keycloak and prepare service for authentication

---
+++ src/main/java/com/poleszak/carrentalauthservice/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.poleszak.carrentalauthservice.config;
+
+import com.poleszak.carrentalauthservice.security.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(GET, "/api/v1/car-rental", "/api/v1/car-rental/**").permitAll()
+                .anyRequest().authenticated();
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+}
