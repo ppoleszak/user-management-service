516186df19b3eada6169e1b759516ba6654bac6c
piotrpoleszak <poleszak@protonmail.com>
2023-04-21 22:20:10 +0200

    Update UserAppRepository

--- src/main/java/com/poleszak/security/user/repository/UserAppRepository.java
+++ src/main/java/com/poleszak/security/user/repository/UserAppRepository.java
@@ -1,4 +1,10 @@
 package com.poleszak.security.user.repository;
 
-public interface UserAppRepository {
+import com.poleszak.security.user.model.UserApp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserAppRepository extends JpaRepository<UserApp, Integer> {
+    Optional<UserApp> findByEmailAndDeletedDateIsNull(String email);
 }
