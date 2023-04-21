5e32151d1cf199814b1334dc0fea42bea441c936
piotrpoleszak <poleszak@protonmail.com>
2023-04-21 22:58:32 +0200

    Implement JwtAuthenticationFilter and JwtService

---
+++ src/main/java/com/poleszak/security/config/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.poleszak.security.config.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractEmail(String jwtToken) {
+        return null;
+    }
+}
