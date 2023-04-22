6f5ee30e0bf19ec963c0434de6a22b89fa04ccc6
piotrpoleszak <poleszak@protonmail.com>
2023-04-22 23:19:55 +0200

    Prepare AuthenticationService

--- src/main/java/com/poleszak/security/auth/service/AuthenticationService.java
+++ src/main/java/com/poleszak/security/auth/service/AuthenticationService.java
@@ -1,9 +1,17 @@
 package com.poleszak.security.auth.service;
 
+import com.poleszak.security.auth.request.AuthenticationRequest;
+import com.poleszak.security.auth.request.RegisterRequest;
+import com.poleszak.security.auth.response.AuthenticationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+    }
+
+    public AuthenticationResponse verify(AuthenticationRequest registerRequest) {
+    }
 }
