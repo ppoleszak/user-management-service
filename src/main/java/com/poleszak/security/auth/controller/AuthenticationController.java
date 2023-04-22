d928614b7463e133e927081708b1f8fb92c6dca3
piotrpoleszak <poleszak@protonmail.com>
2023-04-22 23:18:53 +0200

    Implement auth controller v2

--- src/main/java/com/poleszak/security/auth/controller/AuthenticationController.java
+++ src/main/java/com/poleszak/security/auth/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.poleszak.security.auth.controller;
 import com.poleszak.security.auth.request.AuthenticationRequest;
 import com.poleszak.security.auth.request.RegisterRequest;
 import com.poleszak.security.auth.response.AuthenticationResponse;
+import com.poleszak.security.auth.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
-        //
+        return ResponseEntity.ok().body(authenticationService.register(registerRequest));
     }
 
     @PostMapping("/token")
     public ResponseEntity<AuthenticationResponse> verify(@RequestBody AuthenticationRequest registerRequest) {
-        //
+        return ResponseEntity.ok().body(authenticationService.verify(registerRequest));
     }
 }
